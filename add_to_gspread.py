c617de2324fee13e9bdd3f8a70aa0fd39dcc3b3f
jaeckanaquth <jaeckanaquth@gmail.com>
2021-11-06 22:12:12 +0530

    Added extra css and changed footer a little

--- add_to_gspread.py
+++ add_to_gspread.py
@@ -68,7 +68,7 @@ if config.count == 4:
         worksheet = sh.add_worksheet(title=name, rows="100", cols="7")
     published = pd.DataFrame(worksheet.get_all_records())
     content = '<h3>Chosen from NIFTY 50</h3> <br /> <p><figure>'
-    content = content + published.to_html() + '</figure></p>'
+    content = content + published.to_html(index=False) + '</figure></p>'
     name = "NIFTY 500"
     try:
         worksheet = gc.open('QuthsStocks').worksheet(name)
@@ -76,8 +76,8 @@ if config.count == 4:
         worksheet = sh.add_worksheet(title=name, rows="100", cols="7")
     published = pd.DataFrame(worksheet.get_all_records())
     content = content + '<br /> <body> <h3>Chosen from NIFTY 500</h3> <br /> <p> <figure>'
-    content = content + published.to_html() + "</figure> </p>"
-    content = content + '''<pre>Publisher: QuthCodes </pre> <br /> <footer style="font-size:20%;"> <p>
+    content = content + published.to_html(index=False) + "</figure> </p>"
+    content = content + '''<pre>Publisher: QuthCodes </pre> <br /> <footer> <p>
     The information contained in this website is for general information purposes only. The information is provided by Quths.com and while we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
 
 In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.
