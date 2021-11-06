576889222ce9f8e1fdb3d85339ffd6a5acd250a2
jaeckanaquth <jaeckanaquth@gmail.com>
2021-11-06 21:02:08 +0530

    Version with disclaimer

--- add_to_gspread.py
+++ add_to_gspread.py
@@ -77,15 +77,15 @@ if config.count == 4:
     published = pd.DataFrame(worksheet.get_all_records())
     content = content + '<br /> <body> <h3>Chosen from NIFTY 500</h3> <br /> <p> <figure class="wp-block-table is-style-stripes">'
     content = content + published.to_html() + "</figure> </p>"
-    content = content + '''<code>Publisher: QuthCodes </code> <footer>
-    The information contained in this website is for general information purposes only. The information is provided by [business name] and while we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
+    content = content + '''<pre>Publisher: QuthCodes </pre> <br /> <footer style="font-size:20%;"> <p>
+    The information contained in this website is for general information purposes only. The information is provided by Quths.com and while we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
 
 In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.
 
 Through this website you are able to link to other websites which are not under the control of Quths.com. We have no control over the nature, content and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.
 
-Every effort is made to keep the website up and running smoothly. However, [business name] takes no responsibility for, and will not be liable for, the website being temporarily unavailable due to technical issues beyond our control.
-    </footer>'''
+Every effort is made to keep the website up and running smoothly. However, Quths.com takes no responsibility for, and will not be liable for, the website being temporarily unavailable due to technical issues beyond our control.
+   </p> </footer>'''
     
     try:
         posted = goes_to_wp.posting(content)
